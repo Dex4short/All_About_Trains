db483cb8663e65906074b5f2786d5869a3e457b2
Dex4short <dextepacanya@gmail.com>
2024-09-01 15:50:35 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,27 @@
+
+function nextHtml_content(file){
+	fetch(file)
+		.then(
+			response => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				return response.text();
+			}
+		)
+		.then(
+			data => {
+				document.getElementById('content_pane').innerHTML = data;
+			}
+		)
+		.catch(
+			error => {
+				console.error('Error loading content:', error);
+                document.getElementById('content_pane').innerHTML = '<p align="center">Error loading content. Please try again later.</p>';
+			}
+		);
+}
+
+window.onload = function(){
+	nextHtml_content('home.html');
+}
