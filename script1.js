c9691a895466b58e0d1e8ede01a95c21c10f1cf6
Dex4short <dextepacanya@gmail.com>
2024-09-01 20:38:58 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -42,17 +42,47 @@ document.addEventListener("DOMContentLoaded", function(){
 	
 	home_btn.addEventListener("click", function(){
 		toHome();
+		togleMenuButton(home_btn);
 	});
 	discover_btn.addEventListener("click", function(){
 		toDiscover();
+		togleMenuButton(discover_btn);
 	});
 	about_btn.addEventListener("click", function(){
 		toAbout();
+		togleMenuButton(about_btn);
 	});
 	contactUs_btn.addEventListener("click", function(){
 		toContactUs();
+		togleMenuButton(contactUs_btn);
+	});
+	
+	home_btn.addEventListener("click", function(){
+		
+	});
+	discover_btn.addEventListener("click", function(){
+		
+	});
+	about_btn.addEventListener("click", function(){
+		
+	});
+	contactUs_btn.addEventListener("click", function(){
+		
 	});
 });
+function togleMenuButton(menu_button){
+	home_btn.style.backgroundColor = 'white';
+	home_btn.style.color = 'blick';
+	discover_btn.style.backgroundColor = 'white';
+	discover_btn.style.color = 'blick';
+	about_btn.style.backgroundColor = 'white';
+	about_btn.style.color = 'blick';
+	contactUs_btn.style.backgroundColor = 'white';
+	contactUs_btn.style.color = 'blick';
+	
+	menu_button.style.backgroundColor = '#326B45';
+	menu_button.style.color = 'white';
+}
 
 window.onload = function(){
 	nextHtml_content('home.html');
