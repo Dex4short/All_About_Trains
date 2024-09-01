b042e7a25aa39488e0620512d54f9ae20643db60
Dex4short <dextepacanya@gmail.com>
2024-09-02 02:15:26 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -12,7 +12,7 @@ const locomotives = [
 function load_locomotive_list(){
 	console.log("loading list");
 	
-	const tableBody = document.getElementById('locomotive_list').querySelector('tbody');
+	const tableBody = document.getElementById('locomotive_list');
 	const image_src=0, model_name=1, model_description=2, source_link=3;
 	let item_list = '';
 	
