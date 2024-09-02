1bf82037d9184f748f4848b2fb38b659ed028902
Dex4short <dextepacanya@gmail.com>
2024-09-02 22:07:12 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -19,8 +19,10 @@ function nextHtml_content(file, n){
 		.then(
 			data => {
 				document.getElementById('content_pane').innerHTML = data;
-				if(n == 1){
+				switch(n){
+				case 1:
 					load_locomotive_list('');
+					break
 				}	
 			}
 		)
@@ -56,35 +58,10 @@ function togleMenuButton(menu_button){
 	menu_button.style.backgroundColor = '#326B45';
 	menu_button.style.color = 'white';
 }
-function highlightArea(element, text) {
-	if (element.hasChildNodes()) {
-		element.childNodes.forEach(function(child) {
-			if (child.nodeType === Node.TEXT_NODE) {
-				const parent = child.parentNode;
-				const regex = new RegExp(`(${text})`, 'gi');
-				const newHTML = child.textContent.replace(regex, '<span class="highlight">$1</span>');
-				if (newHTML !== child.textContent) {
-					parent.innerHTML = parent.innerHTML.replace(child.textContent, newHTML);
-					parent.classList.add('highlight');
-				}
-			} else if (child.nodeType === Node.ELEMENT_NODE) {
-			    highlightArea(child, text);
-			}
-		});
-	}
-}
-
 document.addEventListener("DOMContentLoaded", function(){
-	
 	search_btn.addEventListener('click', function() {
 		const key_word = search_bar.value.toLowerCase();
-		if(key_word){
-			document.querySelectorAll('.highlight').forEach( function(element) {
-				element.immerHTML = element.innerHTML.replace(/<span class="highlight">(.*?)<\/span>/g, '$1');
-				element.classList.remove('highlight');
-			});
-			highlightArea(document.body, key_word);
-		}
+		load_locomotive_list(key_word);
 	});
 	
 	home_btn.addEventListener('click', function(){
